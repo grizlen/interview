7f3dbd06b411ae2398f8493c06e09a29cc32d3b4
griz <grizlen@rambler.ru>
2022-04-28 01:11:13 +0500

    upd

--- work2/Main.java
+++ work2/Main.java
@@ -8,10 +8,64 @@ import work2.list.W2List;
 public class Main {
     public static void main(String[] args) {
         // linked list
-        W2List<TestItem> list;
-        list = new W2LinkedList<>();
+        System.out.println("W2LinkedList:");
+        testList(new W2LinkedList<>());
 
         // array list
-        list = new W2ArrayList<>();
+        System.out.println("W2ArrayList:");
+        testList(new W2ArrayList<>());
+    }
+
+    private static void testList(W2List<TestItem> list) {
+        testAdd(list);
+        testIterable(list);
+        testGet(list);
+        testRemove(list);
+        list.print();
+    }
+
+    private static void testAdd(W2List<TestItem> list) {
+        System.out.println("Add:");
+        list.add(-1, new TestItem(1, "Item1"));
+        list.add(new TestItem(3, "Item3"));
+        list.add(1, new TestItem(2, "Item2"));
+        list.add(10, new TestItem(4, "Item4"));
+        list.print();
+    }
+
+    private static void testIterable(W2List<TestItem> list) {
+        System.out.println("For each:");
+        list.forEach(System.out::println);
+    }
+
+    private static void testGet(W2List<TestItem> list) {
+        System.out.println("Get:");
+        try {
+            System.out.println(list.get(-1));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+        System.out.println(list.get(2));
+        System.out.println(list.get(3));
+        try {
+            System.out.println(list.get(4));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void testRemove(W2List<TestItem> list) {
+        System.out.println("Remove:");
+        System.out.println(list.remove(new TestItem(2, "Item2")));
+        System.out.println(list.remove(0));
+        System.out.println(list.remove(0));
+        System.out.println(list.remove(0));
+        try {
+            System.out.println(list.remove(0));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- work2/list/W2ArrayList.java
+++ work2/list/W2ArrayList.java
@@ -1,4 +1,89 @@
 package work2.list;
 
+import java.util.Iterator;
+
 public class W2ArrayList<T> extends W2List<T> {
+
+    private T[] array = (T[]) new Object[1];
+    private int iteratorIndex;
+    private final Iterator<T> iterator = new Iterator<>() {
+        @Override
+        public boolean hasNext() {
+            return iteratorIndex < size;
+        }
+
+        @Override
+        public T next() {
+            if (hasNext()) {
+                return array[iteratorIndex++];
+            }
+            return null;
+        }
+    };
+
+    @Override
+    public void add(T item) {
+        if (size > array.length - 1) {
+            resize();
+        }
+        array[size++] = item;
+    }
+
+    private void resize() {
+        Object[] newArray = new Object[size * 2];
+        System.arraycopy(array, 0, newArray, 0, size);
+        array = (T[]) newArray;
+    }
+
+    @Override
+    public void add(int pos, T item) {
+        if (pos < 0) {
+            pos = 0;
+        }
+        if (pos >= size) {
+            add(item);
+            return;
+        }
+        if (size > array.length - 1) {
+            resize();
+        }
+        System.arraycopy(array, pos, array, pos + 1, size - pos);
+        array[pos] = item;
+        size++;
+    }
+
+    @Override
+    public T get(int pos) {
+        if (pos < 0 || pos >= size) {
+            throw new IndexOutOfBoundsException(pos);
+        }
+        return array[pos];
+    }
+
+    @Override
+    public T remove(int pos) {
+        if (pos < 0 || pos >= size) {
+            throw new IndexOutOfBoundsException(pos);
+        }
+        T result = array[pos];
+        System.arraycopy(array, pos + 1, array, pos, size - pos-1);
+        size--;
+        return result;
+    }
+
+    @Override
+    public T remove(T value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(value)) {
+                return remove(i);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        iteratorIndex = 0;
+        return iterator;
+    }
 }
